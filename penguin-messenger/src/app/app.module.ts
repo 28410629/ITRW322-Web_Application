07482b056b5848e96f2028f67ddeb71fbcc00636
Coenraad Human <coen.human@gmail.com>
2019-07-21 23:08:18 +0200

    Added reset password.

--- penguin-messenger/src/app/app.module.ts
+++ penguin-messenger/src/app/app.module.ts
@@ -21,6 +21,7 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AuthService} from './services/auth.service';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 export function markedOptions(): MarkedOptions {
   const renderer = new MarkedRenderer();
@@ -51,7 +52,8 @@ export function markedOptions(): MarkedOptions {
     SettingsComponent,
     ConversationComponent,
     ProfileComponent,
-    VerifyEmailComponent
+    VerifyEmailComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
